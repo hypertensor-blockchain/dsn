36e9e38a3f6808faf86bf8ad39adcf1aa1b83e3e
hayotensor <hayotensor@gmail.com>
2025-03-13 23:47:01 -0400

    client rpc

--- src/subnet/substrate/consensus.py
+++ src/subnet/substrate/consensus.py
@@ -30,7 +30,8 @@ class AttestReason(Enum):
   ATTESTED = 2
   ATTEST_FAILED = 3
   SHOULD_NOT_ATTEST = 4
-  SHUTDOWN = 5
+  NOT_VALIDATOR = 5
+  SHUTDOWN = 6
 
 class Consensus(threading.Thread):
   """
@@ -255,9 +256,15 @@ class Consensus(threading.Thread):
 
               time.sleep(saturating_sub(delta * BLOCK_SECS, BLOCK_SECS))
               continue
+            elif reason == AttestReason.NOT_VALIDATOR:
+              # Retrieved consensus data for symmetry, but not validator so skipping attestation
+              self.last_validated_or_attested_epoch = epoch
+              break
             elif reason == AttestReason.SHUTDOWN:
               # break and reset loop
+              self.last_validated_or_attested_epoch = epoch
               break
+
             # If False, still waiting for validator to submit data
             continue
           else:
@@ -335,9 +342,11 @@ class Consensus(threading.Thread):
         return True, AttestReason.ATTESTED
       else:
         return False, AttestReason.ATTEST_FAILED
-    else:
-      logger.info("Retrieved data but not able Validator class yet, skipping attestation.")
+    elif should_attest is False and attest:
       return False, AttestReason.SHOULD_NOT_ATTEST
+    else:
+      logger.info("Retrieved data for symmetry but not Validator class yet, skipping attestation.")
+      return False, AttestReason.NOT_VALIDATOR
   
   def is_submittable(self) -> bool:
     submittable_nodes = get_submittable_nodes(
